780ed7c16b9898f9f2f69b1bfcb4d65d9cbcae55
felipe-a-almeida <felipe_a.almeida@outlook.com>
2024-09-25 20:04:21 -0300

    feat: first commit and navbar

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { AboutUs, Chef, FindUs, Footer, Gallery, Header, Intro, Laurels, SpecialMenu } from './container';
+import { Navbar } from './components';
+import './App.css';
+
+const App = () => (
+  <div>
+    <Navbar />
+  </div>
+);
+
+export default App;
